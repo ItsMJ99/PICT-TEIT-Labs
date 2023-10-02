2343d47d6bf06b45aa3c32902448f4adaa9bd972
ItsMJ99 <madhav3jadhav@gmail.com>
2023-10-02 22:33:07 +0530

    n queens

---
+++ DAA/N-Queens.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool isSafe(int row, int col, vector<string> board, int n)
+{
+    int storeCol = col;
+    int storeRow = row;
+    while (row >= 0 && col >= 0)
+    {
+        if (board[row][col] == 'Q')
+            return false;
+        row--;
+        col--;
+    }
+
+    row = storeRow;
+    col = storeCol;
+    while (col >= 0)
+    {
+        if (board[row][col] == 'Q')
+            return false;
+        col--;
+    }
+
+    row = storeRow;
+    col = storeCol;
+    while (row < n && col >= 0)
+    {
+        if (board[row][col] == 'Q')
+            return false;
+        row++;
+        col--;
+    }
+
+    return true;
+}
+
+void solve(int col, vector<string> &board, vector<vector<string>> &ans, int n)
+{
+    if (col == n)
+    {
+        ans.push_back(board);
+        return;
+    }
+    for (int row = 0; row < n; row++)
+    {
+        if (isSafe(row, col, board, n))
+        {
+            board[row][col] = 'Q';
+            solve(col + 1, board, ans, n);
+            board[row][col] = '.';
+        }
+    }
+}
+
+void solveNQueens(int n)
+{
+    vector<vector<string>> ans;
+    vector<string> board(n, string(n, '.'));
+    solve(0, board, ans, n);
+
+    int op = ans.size();
+    if (op > 5)
+        op = 5;
+
+    for (int i = 0; i < op; i++)
+    {
+        cout << "Solution " << i + 1 << ":\n";
+        for (int j = 0; j < n; j++)
+        {
+            cout << "|";
+            for (int k = 0; k < n; k++)
+            {
+                if (ans[i][j][k] == 'Q')
+                    cout << "Q ";
+                else
+                    cout << ". ";
+            }
+            cout << "|" << endl;
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number of Queens : ";
+    cin >> n;
+    solveNQueens(n);
+    return 0;
+}
