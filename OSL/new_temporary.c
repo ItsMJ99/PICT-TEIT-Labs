d3e3faba8f4c1307b1dfdfa3855e6fd2e05fa659
MJ <95184642+ItsMJ99@users.noreply.github.com>
2023-11-07 15:45:00 +0530

    osl a5

--- OSL/new_temporary.c
+++ OSL/new_temporary.c
@@ -1,75 +1,102 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<pthread.h>
-#include<semaphore.h>
-#include<unistd.h>
+ #include<stdio.h>
+#include<stdbool.h>
 
-sem_t mutex;
-sem_t wrt;
-int readcount=0,nwt,nrd;
+struct Process{
+    int pid;
+    int allocated[3];
+    int maxneed[3];
+    int remneed[3];
+    bool cs;
+};
 
-void *reader_thread(void *temp){
-	int *id_ptr=(int *)temp;
-	int id=*id_ptr;
-	free(id_ptr);
-	printf("Reader %d is trying to enter the Database for reading\n",id);
-	sem_wait(&mutex);
-	readcount++;
-	if(readcount==1)
-		sem_wait(&wrt);
-	sem_post(&mutex);
-
-	printf("Reader %d is reading from the database\n",id);
-	sem_wait(&mutex);
-	readcount--;
-	if(readcount==0)
-		sem_post(&wrt);
-	sem_post(&mutex);
-	printf("Reader %d left the Database\n",id);
-	sleep(5);
-	pthread_exit(NULL);
+void showTable(struct Process p[],int n){
+     printf("PID\tAllocated\tMax Need\tRemaining Need\n");
+    printf("\tA  B  C         A  B  C         A  B  C\n");
+    for(int i=0;i<n;i++){
+        printf(" %d\t",i+1);
+        for(int j=0;j<3;j++){
+            printf("%d  ",p[i].allocated[j]);
+        }
+        printf("\t");
+        for(int k=0;k<3;k++){
+            printf("%d  ",p[i].maxneed[k]);
+        }
+        printf("\t");
+        for(int l=0;l<3;l++){
+            printf("%d  ",p[i].remneed[l]);
+        }
+        printf("\n");
+    } 
 }
 
-void *writer_thread(void *temp){
-	int *id_ptr=(int *)temp;
-	int id=*id_ptr;
-	free(id_ptr);
-	printf("Writer %d is trying to enter the Database for writing\n",id);
-	sem_wait(&wrt);
-	printf("Writer %d is now writing in the Database\n",id);
-	sleep(5);
-	printf("Writer %d left the Database\n",id);
-	sem_post(&wrt);
-	pthread_exit(NULL);
+void safetyAlgorithm(struct Process p[],int n,int taa,int tab,int tac){
+    int safeSequence[n];
+    int al=0;
+    int completed=0;
+    int a,b,c;
+    printf("Enter total of A Resource : ");
+    scanf("%d",&a);
+    printf("Enter total of B Resource : ");
+    scanf("%d",&b);
+    printf("Enter total of C Resource : ");
+    scanf("%d",&c);
+    taa=a-taa;
+    tab=a-tab;
+    tac=a-tac;
+    while(completed!=n){
+        for(int i=0;i<n;i++){
+            if(!p[i].cs && p[i].remneed[0]<=taa && p[i].remneed[1]<=tab && p[i].remneed[2]<=tac){
+                p[i].cs=true;
+                taa+=p[i].allocated[0];
+                tab+=p[i].allocated[1];
+                tac+=p[i].allocated[2];
+                safeSequence[al]=p[i].pid;
+                al++;
+                completed++;
+            }
+            if(i==n-1 && al==0){
+                printf("Deadlock will occur in this Allocations");
+            }
+        }
+    }
+    printf("\n\nSafety Sequence : ");
+    for(int i=0;i<n;i++){
+        printf("%d",safeSequence[i]);
+        if(i<n-1){
+            printf("->");
+        }
+    }
+    showTable(p,n);
 }
 
-int main(){
-	long int i;
-	sem_init(&mutex,0,1);
-	sem_init(&wrt,0,1);
-	pthread_t reader[100],writer[100];
-	printf("\nEnter the number of Writers : ");
-	scanf("%d",&nwt);
-	printf("\nEnter the number of Readers : ");
-	scanf("%d",&nrd);
-	for(i=1;i<=nwt;i++){
-		int *writer_id=malloc(sizeof(int));
-		*writer_id=i;
-		pthread_create(&writer[i],NULL,writer_thread,(void *)writer_id);
-	}
-	for(i=1;i<=nrd;i++){
-		int *reader_id=malloc(sizeof(int));
-		*reader_id=i;
-		pthread_create(&reader[i],NULL,reader_thread,(void *)reader_id);
-	}
-	for(i=1;i<=nwt;i++){
-		pthread_join(writer[i],NULL);
-	}
-	for(i=1;i<=nrd;i++){
-		pthread_join(reader[i],NULL);
-	}
-	sem_destroy(&wrt);
-	sem_destroy(&mutex);
-	pthread_exit(NULL); // Exit the main thread gracefully
-	return 0;
-}
+ int main(){
+    int n,taa=0,tab=0,tac=0;
+    printf("Enter the number of processes : ");
+    scanf("%d",&n);
+    struct Process p[n];
+    for(int i=0;i<n;i++){
+        p[i].pid=i+1;
+        printf("Enter Process %d Allocated Resources (A,B,C) : ",i+1);
+        for(int j=0;j<3;j++){
+            scanf("%d",&p[i].allocated[j]);
+            if(j==0){
+                taa+=p[i].allocated[j];
+            }
+            else if(j==1){
+                tab+=p[i].allocated[j];
+            }
+            else  {  
+                tac+=p[i].allocated[j];
+            }
+        }
+        printf("Enter Process %d Maximum Need of Resources (A,B,C) : ",i+1);
+        for(int k=0;k<3;k++){
+            scanf("%d",&p[i].maxneed[k]);
+            p[i].remneed[i]=p[i].maxneed[k]-p[i].allocated[k];
+        }
+        p[i].cs=false;
+    }
+    safetyAlgorithm(p,n,taa,tab,tac);
+    showTable(p,n);  
+    return 0;
+ }
